665b324bf8543111d3b89d99b7842b54ff988a76
adamshiervani <adam.shiervani@gmail.com>
2013-05-23 13:23:46 +0200

    Clean. Bort med sliding menu

--- src/com/example/tig063vgr/MainActivity.java
+++ src/com/example/tig063vgr/MainActivity.java
@@ -11,19 +11,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-        // set the content view
         setContentView(R.layout.activity_main);
-        // configure the SlidingMenu
-        
-        SlidingMenu menu = new SlidingMenu(this);
-        menu.setMode(SlidingMenu.LEFT);
-        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
-        menu.setShadowWidthRes(R.dimen.shadow_width);
-        menu.setShadowDrawable(R.drawable.shadow);
-        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
-        menu.setFadeDegree(0.35f);
-        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
-        menu.setMenu(R.layout.menu);
 
 	}
 
