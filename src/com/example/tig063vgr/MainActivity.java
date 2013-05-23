b9936126dbd7344b84e52d072bb6d0779e198f36
Rasmus Jansson <rallejansson37@gmail.com>
2013-05-23 14:17:38 +0200

    messing around big time

--- src/com/example/tig063vgr/MainActivity.java
+++ src/com/example/tig063vgr/MainActivity.java
@@ -3,16 +3,28 @@ package com.example.tig063vgr;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
+import modality.ModalityServiceAgent;
+import org.datacontract.schemas._2004._07.vgregion_modalities_wcf_data.ArrayOfEquipmentDto;
+import org.datacontract.schemas._2004._07.vgregion_modalities_wcf_data.EquipmentDto;
 
-import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import javax.xml.bind.JAXBElement;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
+    TextView v;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        v = (TextView)findViewById(R.id.textView1);
+        ArrayOfEquipmentDto a = ModalityServiceAgent.getInstance().getService().getEquipments();
+        List<EquipmentDto> lista = a.getEquipmentDto();
+        for (int i=0; i<lista.size();i++) {
+            //JAXBElement<String> element;
+            v.setText(v.getText() + "\n" + lista.get(i).getName().getValue());
+        }
 	}
 
 	@Override
