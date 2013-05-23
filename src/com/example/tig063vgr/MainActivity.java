d7e594e93875e546e92baac5339416e6b0773647
adamshiervani <adam.shiervani@gmail.com>
2013-05-23 14:19:03 +0200

    asd

--- src/com/example/tig063vgr/MainActivity.java
+++ src/com/example/tig063vgr/MainActivity.java
@@ -3,16 +3,12 @@ package com.example.tig063vgr;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-
-import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
-
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 	}
 
 	@Override
