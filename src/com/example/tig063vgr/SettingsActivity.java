183d625e05cec87f0032c07965070439a48a88d8
adamshiervani <adam.shiervani@gmail.com>
2013-05-24 17:29:45 +0200

    fungerande inställnigar. Stabil version. Enkel funktionalitet

--- src/com/example/tig063vgr/SettingsActivity.java
+++ src/com/example/tig063vgr/SettingsActivity.java
@@ -1,9 +1,14 @@
 package com.example.tig063vgr;
 
+import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 public class SettingsActivity extends PreferenceActivity {
-	
-	
-	
+	@SuppressWarnings("deprecation")
+	@Override
+	public void onCreate(Bundle saveInstances) {
+		super.onCreate(saveInstances);
+		addPreferencesFromResource(R.xml.settings);
+	}
+
 }
