358320374819d311a80bf3dda2d2c4daf872bcec
Rasmus Jansson <rallejansson37@gmail.com>
2013-05-27 13:51:30 +0200

    lsa

---
+++ src/com/example/tig063vgr/ListViewAdapter.java
@@ -0,0 +1,31 @@
+package com.example.tig063vgr;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by rasmus on 2013-05-27.
+ */
+public class ListViewAdapter extends BaseAdapter{
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
