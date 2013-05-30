2b5ebb8ae98a44f928c0756992399d51b378e294
Rasmus Jansson <rallejansson37@gmail.com>
2013-05-30 12:47:05 +0200

    JSON-API implementet

--- src/com/example/tig063vgr/fragments/FragmentMaskin.java
+++ src/com/example/tig063vgr/fragments/FragmentMaskin.java
@@ -3,7 +3,9 @@ package com.example.tig063vgr.fragments;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -13,6 +15,10 @@ import android.widget.ListView;
 
 import com.example.tig063vgr.R;
 import com.example.tig063vgr.adapter.ListViewAdapter;
+import com.example.tig063vgr.httprules.Modality;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class FragmentMaskin extends Fragment {
 
@@ -24,52 +30,67 @@ public class FragmentMaskin extends Fragment {
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.activity_maskin, container,
 				false);
+        Modality modality = new Modality(getActivity());
+        JSONObject equipment = null;
+        try {
+            JSONArray result = modality.GetEquipments().get();
+            Random r = new Random();
+            equipment  = result.getJSONObject(r.nextInt(result.length()));
 
-		// Init Vy and header
-		ListView rapportList = (ListView) rootView
-				.findViewById(R.id.listRapport);
-		View header = inflater.inflate(R.layout.rapport_listheader, null);
-		rapportList.addHeaderView(header);
 
-		// Populate hashmap
-		ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
-		HashMap<String, String> item = new HashMap<String, String>();
-		item.put("large", "Utrustning ID");
-		item.put("small", "EQUIPMENT_ID");
-		list.add(item);
+            // Init Vy and header
+            ListView rapportList = (ListView) rootView
+                    .findViewById(R.id.listRapport);
+            View header = inflater.inflate(R.layout.rapport_listheader, null);
+            rapportList.addHeaderView(header);
 
-		item = new HashMap<String, String>();
-		item.put("large", "Namn");
-		item.put("small", "NAME");
-		list.add(item);
+            // Populate hashmap
+            ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
+            HashMap<String, String> item = new HashMap<String, String>();
+            item.put("large", "Utrustning ID");
+            item.put("small", equipment.getString("Id"));
+            list.add(item);
 
-		item = new HashMap<String, String>();
-		item.put("large", "Tillverkare");
-		String[] menyItems = { " Simens", "Nokia", "Volvo", "SAAB", "Intel",
-				"AMD", "Nvidia", "LG", "Apple", "Dell", "Microsoft", "Google",
-				"NASA" };
-		Random r = new Random();
-		item.put("small", menyItems[r.nextInt(menyItems.length)]);
-		list.add(item);
+            item = new HashMap<String, String>();
+            item.put("large", "Namn");
+            item.put("small", equipment.getString("Name"));
+            list.add(item);
 
-		item = new HashMap<String, String>();
-		item.put("large", "Plats");
-		item.put("small", "LOCATION_ID");
-		list.add(item);
+            item = new HashMap<String, String>();
+            item.put("large", "Tillverkare");
+            String[] menyItems = { " Simens", "Nokia", "Volvo", "SAAB", "Intel",
+                    "AMD", "Nvidia", "LG", "Apple", "Dell", "Microsoft", "Google",
+                    "NASA" };
 
-		item = new HashMap<String, String>();
-		item.put("large", "Status");
-		item.put("small", "STAUTS_ID");
-		list.add(item);
+            item.put("small", menyItems[r.nextInt(menyItems.length)]);
+            list.add(item);
 
-		item = new HashMap<String, String>();
-		item.put("large", "Modality Group");
-		item.put("small", "MOD_GROUP");
-		list.add(item);
+            item = new HashMap<String, String>();
+            item.put("large", "Plats");
+            item.put("small", equipment.getString("LocationId"));
+            list.add(item);
 
-		ListViewAdapter adapter = new ListViewAdapter(getActivity(), list);
-		rapportList.setAdapter(adapter);
 
+            //TODO: Hämta riktig status
+            item = new HashMap<String, String>();
+            item.put("large", "Status");
+            item.put("small", "Påbörjad");
+            list.add(item);
+
+            item = new HashMap<String, String>();
+            item.put("large", "Modality Group");
+            item.put("small", equipment.getString("ModalityGroupId"));
+            list.add(item);
+
+            ListViewAdapter adapter = new ListViewAdapter(getActivity(), list);
+            rapportList.setAdapter(adapter);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 		return rootView;
 	}
 }
