aad428458e393be952ff9ad1f1fbc17e6b6df369
Rasmus Jansson <rallejansson37@gmail.com>
2013-06-03 13:08:20 +0200

    ..

--- src/com/example/tig063vgr/MainActivity.java
+++ src/com/example/tig063vgr/MainActivity.java
@@ -1,69 +1,86 @@
 package com.example.tig063vgr;
 
-import java.util.Random;
-import java.util.concurrent.ExecutionException;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.view.ViewPager;
+import android.view.View;
 import android.widget.ImageButton;
-
 import com.example.tig063vgr.adapter.SlidePagerAdapter;
 import com.example.tig063vgr.animations.ZoomOutPageTransformer;
 import com.example.tig063vgr.httprules.Modality;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.Random;
 
 public class MainActivity extends UiInit {
 
-	SlidePagerAdapter mSectionsPagerAdapter;
-	ViewPager mViewPager;
-	private static final int PICTURE_RESULT = 1337;
-	SoundRecorder record;
+    public SlidePagerAdapter mSectionsPagerAdapter;
+    ViewPager mViewPager;
+    private static final int PICTURE_RESULT = 1337;
+    SoundRecorder record;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		setContentView(R.layout.viewpagertop);
-		super.onCreate(savedInstanceState);
-		// TODO: Får följande INFO debug Bitmap too large to be uploaded into a
-		// texture (2362x556, max=2048x2048)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.viewpagertop);
+        super.onCreate(savedInstanceState);
+        // TODO: Får följande INFO debug Bitmap too large to be uploaded into a
+        // texture (2362x556, max=2048x2048)
 
-		mSectionsPagerAdapter = new SlidePagerAdapter(
-				getSupportFragmentManager());
-		mViewPager = (ViewPager) findViewById(R.id.pager);
-		mViewPager.setPageTransformer(true, new ZoomOutPageTransformer());
+        mSectionsPagerAdapter = new SlidePagerAdapter(
+                getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setPageTransformer(true, new ZoomOutPageTransformer());
 
         Modality modality = new Modality(this);
-
-
-        //DEN HÄR
         modality.GetEquipments();
-        //DEN HÄR
-
-
     }
 
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
+    public void btnScanNewClick(View v) {
+        Intent m = new Intent(this, ScanNfc.class);
+        m.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(m);
+        finish();
+    }
 
-		if (requestCode == PICTURE_RESULT) {
-			if (resultCode == Activity.RESULT_OK) {
-				Bundle b = data.getExtras();
-				Bitmap bmp = (Bitmap) b.get("data");
-				if (bmp != null) {
-					ImageButton cameraBtn = (ImageButton) findViewById(R.id.buttonCamera);
-					cameraBtn.setImageResource(R.drawable.cameraselected);
-					cameraBtn.setEnabled(false);
+    public void btnSendClick(View v) {
+        final ProgressDialog pd = new ProgressDialog(this);
+        pd.setMessage("Skickar rapport");
+        pd.show();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                pd.dismiss();
+                mViewPager.setCurrentItem(mViewPager.getCurrentItem() + 1, true);
+            }
+        }, 3000);
+    }
 
-				}
-			}
-		}
+    public void btnNextClick(View v) {
+        mViewPager.setCurrentItem(mViewPager.getCurrentItem() + 1, true);
+    }
 
-	}
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICTURE_RESULT) {
+            if (resultCode == Activity.RESULT_OK) {
+                Bundle b = data.getExtras();
+                Bitmap bmp = (Bitmap) b.get("data");
+                if (bmp != null) {
+                    ImageButton cameraBtn = (ImageButton) findViewById(R.id.buttonCamera);
+                    cameraBtn.setImageResource(R.drawable.cameraselected);
+                    cameraBtn.setEnabled(false);
+
+                }
+            }
+        }
+
+    }
 
     public void setData(JSONArray result) {
 
--- src/com/example/tig063vgr/fragments/FragmentMaskin.java
+++ src/com/example/tig063vgr/fragments/FragmentMaskin.java
@@ -1,82 +1,85 @@
 package com.example.tig063vgr.fragments;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Random;
-
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
-
 import com.example.tig063vgr.Equipment;
 import com.example.tig063vgr.R;
 import com.example.tig063vgr.adapter.ListViewAdapter;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
 @SuppressLint("ValidFragment")
 public class FragmentMaskin extends Fragment {
-	public Equipment equipment;
+    private Equipment equipment;
+    private Button btnNextMaskin;
 
-	public FragmentMaskin(Equipment e) {
-		equipment = e;
-	}
+    public FragmentMaskin(Equipment e) {
+        equipment = e;
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+    }
 
-		View rootView = inflater.inflate(R.layout.activity_maskin, container,
-				false);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
-		Random r = new Random();
-		// Init Vy and header
-		ListView rapportList = (ListView) rootView
-				.findViewById(R.id.listRapport);
-		View header = inflater.inflate(R.layout.rapport_listheader, null);
-		rapportList.addHeaderView(header);
+        View rootView = inflater.inflate(R.layout.activity_maskin, container,
+                false);
 
-		// Populate hashmap
-		ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
-		HashMap<String, String> item = new HashMap<String, String>();
-		item.put("large", "Utrustning ID");
-		item.put("small", equipment.getId());
-		list.add(item);
+        Random r = new Random();
+        // Init Vy and header
+        ListView rapportList = (ListView) rootView
+                .findViewById(R.id.listRapport);
+        View header = inflater.inflate(R.layout.rapport_listheader, null);
+        rapportList.addHeaderView(header);
 
-		item = new HashMap<String, String>();
-		item.put("large", "Namn");
-		item.put("small", equipment.getName());
-		list.add(item);
+        // Populate hashmap
+        ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
+        HashMap<String, String> item = new HashMap<String, String>();
+        item.put("large", "Utrustning ID");
+        item.put("small", equipment.getId());
+        list.add(item);
 
-		item = new HashMap<String, String>();
-		item.put("large", "Tillverkare");
-		String[] menyItems = { "GE Med.", "Scan OD", "Medicine Radio",
-				"Hospital Radiology Medicine" };
+        item = new HashMap<String, String>();
+        item.put("large", "Namn");
+        item.put("small", equipment.getName());
+        list.add(item);
 
-		item.put("small", menyItems[r.nextInt(menyItems.length)]);
-		list.add(item);
+        item = new HashMap<String, String>();
+        item.put("large", "Tillverkare");
+        String[] menyItems = {"GE Med.", "Scan OD", "Medicine Radio",
+                "Hospital Radiology Medicine"};
 
-		item = new HashMap<String, String>();
-		item.put("large", "Plats");
-		item.put("small", equipment.getLocationId());
-		list.add(item);
+        item.put("small", menyItems[r.nextInt(menyItems.length)]);
+        list.add(item);
 
-		// TODO: Hämta riktig status
-		item = new HashMap<String, String>();
-		item.put("large", "Status");
-		item.put("small", "Påbörjad");
-		list.add(item);
+        item = new HashMap<String, String>();
+        item.put("large", "Plats");
+        item.put("small", equipment.getLocationId());
+        list.add(item);
 
-		item = new HashMap<String, String>();
-		item.put("large", "Modality Group");
-		item.put("small", equipment.getModalityGroupId());
-		list.add(item);
+        // TODO: Hämta riktig status
+        item = new HashMap<String, String>();
+        item.put("large", "Status");
+        item.put("small", "Påbörjad");
+        list.add(item);
 
-		ListViewAdapter adapter = new ListViewAdapter(getActivity(), list);
-		rapportList.setAdapter(adapter);
+        item = new HashMap<String, String>();
+        item.put("large", "Modality Group");
+        item.put("small", equipment.getModalityGroupId());
+        list.add(item);
 
-		return rootView;
-	}
+        ListViewAdapter adapter = new ListViewAdapter(getActivity(), list);
+        rapportList.setAdapter(adapter);
+
+        return rootView;
+    }
 }
